70900de1a779d93dea9420b463797f549b804bfc
syzer <syzer3@gmail.com>
2015-05-19 10:30:54 +0200

    fix for new url

--- peerflixKickass.js
+++ peerflixKickass.js
@@ -2,6 +2,7 @@
 // @name        kickass peerflix
 // @namespace   Syzer
 // @include     https://kickass.to/*
+// @include     http://kat.cr/*
 // @version     1
 // @grant       none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
