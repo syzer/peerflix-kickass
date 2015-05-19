9c79b21bb702360ae360e01af08c9363ced87316
syzer <syzer3@gmail.com>
2015-05-19 10:43:39 +0200

    refactor

--- peerflixKickass.js
+++ peerflixKickass.js
@@ -8,14 +8,19 @@
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // ==/UserScript==
 
-$( document ).ready(function() {
-    var mine = $('.magnetlinkButton').clone().insertAfter('.magnetlinkButton');
+var button = '.magnetlinkButton';
 
-    var command = 'peerflix --vlc -f"./" "' + mine.attr('href') + '"';
+function addButton(button) {
+    var newButton = $(button).clone().insertAfter(button).html('peerflix');
 
-    mine.click(function(e) {
+    var command = 'peerflix --vlc -f"./" "' + newButton.attr('href') + '"';
+
+    newButton.click(function(e) {
         window.prompt("Ctrl+C, Enter", command);
         e.preventDefault();
     })
+}
 
+$( document ).ready(function() {
+    addButton(button);
 });
