0eefd91d3348e2b97a5d59f19bde3ff67b632eb8
syzer3@gmail.com <syzer3@gmail.com>
2017-08-02 09:09:20 +0200

    formatting

--- peerflixKickass.js
+++ peerflixKickass.js
@@ -8,19 +8,19 @@
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // ==/UserScript==
 
-var button = '.magnetlinkButton';
+var button = '.magnetlinkButton'
 
 function addButton(button) {
-    var newButton = $(button).clone().insertAfter(button).html('peerflix');
+    var newButton = $(button).clone().insertAfter(button).html('peerflix')
 
-    var command = 'peerflix --vlc -f"./" "' + newButton.attr('href') + '"';
+    var command = 'peerflix --vlc -f"./" "' + newButton.attr('href') + '"'
 
     newButton.click(function(e) {
-        window.prompt("Ctrl+C, Enter", command);
-        e.preventDefault();
+        window.prompt("Ctrl+C, Enter", command)
+        e.preventDefault()
     })
 }
 
 $( document ).ready(function() {
-    addButton(button);
-});
+    addButton(button)
+})
